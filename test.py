4fad7d4d24b7b064900ae2666ee1bbe5e9c08569
ItaloSilva <italo.silva.silvera@outlook.com>
2024-01-11 09:50:28 -0500

    only full backup

--- test.py
+++ test.py
@@ -13,64 +13,38 @@ PG_DATABASE = os.environ.get("PG_DATABASE")
 PG_USER = os.environ.get("PG_USER")
 PGPASSWORD = os.environ.get("PGPASSWORD")
 FECHAYHORA = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-PG_FILENAME = f"./backup_logs_{FECHAYHORA}.csv"
 PG_BACKUP_FILENAME = f"./fullbackup_{PG_DATABASE}_{FECHAYHORA}.backup"
 
 try:
-    conn = psycopg2.connect(
-        host=PG_HOST,
-        port=PG_PORT,
-        dbname=PG_DATABASE,
-        user=PG_USER,
-        password=PGPASSWORD
-    )
-
     # Realizar Full Backup
 
     command = [
-    "pg_dump",
-    "-h", PG_HOST,
-    "-p", str(PG_PORT),
-    "-U", PG_USER,
-    "-F", "c",  
-    "-f", PG_BACKUP_FILENAME,  
-    "-d", PG_DATABASE 
+        "pg_dump",
+        "-h", PG_HOST,
+        "-p", str(PG_PORT),
+        "-U", PG_USER,
+        "-F", "c",  
+        "-f", PG_BACKUP_FILENAME,  
+        "-d", PG_DATABASE 
     ]
     # Establecer la variable de entorno PGPASSWORD
     os.environ["PGPASSWORD"] = PGPASSWORD
 
     subprocess.run(command, check=True)
-    
-    # Crear CSV basado en query
-
-    cur = conn.cursor()
-
-    query_export = """
-    COPY (
-        SELECT * FROM public.log WHERE fecharegistro >= date_trunc('MONTH', current_date - INTERVAL '2 MONTH')
-        AND fecharegistro < date_trunc('MONTH', current_date);)
-        TO STDOUT WITH CSV HEADER DELIMITER '|'
-    """
-
-    with open(PG_FILENAME, 'w') as f:
-        cur.copy_expert(query_export, f)
-
-    cur.close()
-    conn.close()
+    print("Backup completo realizado con éxito")
 
 except Exception as e:
-    print("Error durante la exportacion desde PostgreSQL:", e)
+    print("Error durante el backup de PostgreSQL:", e)
 
-# Enviar full backup y archivo CSV al COS
+# Enviar full backup al COS
 
 APIKEY = os.environ.get("APIKEY")
 ENDPOINT = os.environ.get("ENDPOINT")
 SERVICE_INSTANCE_ID = os.environ.get("SERVICE_INSTANCE_ID")
 BUCKET_NAME = os.environ.get("BUCKET_NAME")
-OBJECT_NAME = f"backup_{PG_DATABASE}_{FECHAYHORA}.csv"
 BACKUP_OBJECT_NAME = f"fullbackup_{PG_DATABASE}_{FECHAYHORA}.backup"
 
-# Configuracion del cliente para IBM COS
+# Configuración del cliente para IBM COS
 
 try:
 
@@ -83,11 +57,9 @@ try:
 
     with open(PG_BACKUP_FILENAME, "rb") as file_data:
         cos.Object(BUCKET_NAME, BACKUP_OBJECT_NAME).upload_fileobj(file_data)
-
-    with open(PG_FILENAME, "rb") as file_data:
-        cos.Object(BUCKET_NAME, OBJECT_NAME).upload_fileobj(file_data)
+    print("Backup subido con éxito a IBM COS")
 
 except Exception as e:
-    print("Un error ocurrió:", e)
+    print("Un error ocurrió durante la subida a IBM COS:", e)
 
 del os.environ["PGPASSWORD"]
