f564782ead95abe83c9c2bae840e4b73c067ddf8
ItaloSilva <italo.silva.silvera@outlook.com>
2024-02-10 03:07:35 -0500

    porfavor

--- test.py
+++ test.py
@@ -85,7 +85,7 @@ try:
         "-f", PG_BACKUP_FILENAME,
         "-d", PG_DATABASE
     ]
-    PGPASSWORD = PGPASSWORD
+    secretos['PGPASSWORD'] = PGPASSWORD
     subprocess.run(command, check=True)
     print("Backup completo realizado con éxito")
 
